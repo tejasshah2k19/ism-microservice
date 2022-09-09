50d8692aa938f584f1bdb23ed1a950b29a097e74
tejas <tejasshah@gmail.com>
2022-09-09 11:10:44 +0530

    eureka server for service registry

--- service-registry/src/main/java/com/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/ServiceRegistryApplication.java
@@ -3,13 +3,14 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class DepartmentServiceApplication {
+@EnableEurekaServer
+public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DepartmentServiceApplication.class, args);
+		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
 
 }
